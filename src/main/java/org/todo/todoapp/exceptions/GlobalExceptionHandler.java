26b8589e04cb1b1a82fe26a4a9b28c119f3574a9
oPelayo <120644876+oPelayo@users.noreply.github.com>
2025-04-09 12:41:35 +0200

    Exception handling

---
+++ src/main/java/org/todo/todoapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.todo.todoapp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/org/todo/todoapp/exceptions/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package org.todo.todoapp.exceptions;
+
+public class TaskNotFoundException extends RuntimeException{
+    public TaskNotFoundException(String message){
+        super(message);
+    }
+}
