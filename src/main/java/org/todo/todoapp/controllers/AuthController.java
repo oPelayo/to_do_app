fa0abcf6c79c12f65012439e084ef50fc7076369
oPelayo <120644876+oPelayo@users.noreply.github.com>
2025-04-09 12:36:19 +0200

    Implementation AuthController

---
+++ src/main/java/org/todo/todoapp/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package org.todo.todoapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.todo.todoapp.model.User;
+import org.todo.todoapp.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        String token = authService.login(user.getUsername(), user.getPassword());
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User registeredUser = authService.register(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+}
